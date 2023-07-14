b82f96a21f52a23963bae0c97d5ee200b8084fe0
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-14 21:15:58 +0700

    Create text.py

---
+++ DzStore/text.py
@@ -0,0 +1,65 @@
+NOKOS = """**
+NOKOS TELEGRAM INDONESIA🇮🇩
+
+List:
+1.Nokos id 1 price Rp 40.000
+2.Nokos id 2 price Rp 35.000
+3.Nokos id fresh 5/6 price Rp 5.000
+
+Untuk membeli silahkan klik
+tombol beli dibawah ini⚠**
+"""
+
+NOKOS_SET = """
+```
+#BELI_NOKOS_TELEGRAM
+
+Jenis Nokos id = ... 
+Harga Nokos = ...
+Pembayaran = ... 
+```
+Pilih jenis dan
+metode pembayaran✅
+"""
+
+NOKOS_JENIS = "**Silahkan pilih jenis nokos yang di inginkan**"
+
+NOKOS1 = """
+```
+#BELI_NOKOS_TELEGRAM
+
+Jenis Nokos id = 1
+Harga Nokos = Rp 40.000
+Pembayaran = ... 
+```
+Pilih metode pembayaran✅
+***
+"""
+
+NOKOS2 = """
+```
+#BELI_NOKOS_TELEGRAM
+
+Jenis Nokos id = 2
+Harga Nokos = Rp 35.000
+Pembayaran = ... 
+```
+Pilih metode pembayaran✅
+***
+"""
+
+NOKOS_FRESH = """
+```
+#BELI_NOKOS_TELEGRAM
+
+Jenis Nokos id = FRESH 5/6
+Harga Nokos = Rp 5.000
+Pembayaran = ... 
+```
+Pilih metode pembayaran✅
+***
+"""
+
+NOKOS_PAY = "**Silahkan pilih metode pembayaran yang diinginkan**"
+
+NOKOS_PAY1 = """
